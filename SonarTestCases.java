f56b2d0bcac8d21a7adb4e90fe97eae98b52b753
a.shamjith@gmail.com <shamjith.antholi@shamjiths-iMac.local>
2019-01-13 20:11:40 -0500

    modified test cases2

---
+++ SonarTestCases.java
@@ -0,0 +1,22 @@
+package com.example.test.junit;
+
+import com.example.test.sample.SimpleWordCounter;
+
+import junit.framework.TestCase;
+
+public class SonarTestCases extends TestCase{
+
+	public void testWordCounterSuccess() {
+		SimpleWordCounter simpleWordCounter = new SimpleWordCounter();
+		System.out.println("Testing 1........>>>");
+		assertEquals(35, simpleWordCounter.CountWordsInFile());
+		
+	}
+	public void testWordCounterFail() {
+		SimpleWordCounter simpleWordCounter = new SimpleWordCounter();
+		System.out.println("Testing 21........>>>");
+		assertEquals(25, simpleWordCounter.CountWordsInFile());
+		
+	}
+
+}
