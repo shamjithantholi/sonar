0465ddc96d60cbaf07af40255b23adc17c47744a
a.shamjith@gmail.com <shamjith.antholi@shamjiths-iMac.local>
2019-01-13 21:07:43 -0500

    modified test cases5

--- src/test/java/com/example/test/SonarTestApplicationTests.java
+++ src/test/java/com/example/test/SonarTestApplicationTests.java
@@ -1,9 +1,13 @@
 package com.example.test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.test.sample.SimpleWordCounter;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,5 +17,19 @@ public class SonarTestApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testWordCounterSuccess() {
+		SimpleWordCounter simpleWordCounter = new SimpleWordCounter();
+		System.out.println("Testing 1........>>>");
+		assertEquals(35, simpleWordCounter.CountWordsInFile());
+		
+	}
+	@Test
+	public void testWordCounterFail() {
+		SimpleWordCounter simpleWordCounter = new SimpleWordCounter();
+		System.out.println("Testing 21........>>>");
+		assertEquals(25, simpleWordCounter.CountWordsInFile());
+		
+	}
 }
 
